10788a512c1725626aed2120bb74ab2fc55af28c
alhernand <kasashy@gmail.com>
2017-03-08 21:28:44 -0500

    Add files via upload
    Se sube código de ejecución del Sp de consulta de usuarios por tipo y por numero de identificacion

---
+++ AlexanderHernandez/SP_consulta_usuario_por_tipo_numero_documentoTEST.sql
@@ -0,0 +1,26 @@
+select * from SP_consulta_usuario_por_tipo_numero_documento('CC','12345680')as usuario(
+idusuario integer
+,idtipodocumento integer
+,idrol integer
+,numerodocumento varchar
+,usuario varchar
+,clave varchar
+,nombre varchar
+,apellido varchar
+,estado integer
+,idtipodocumento2 integer
+,tipo varchar
+,descripcion varchar
+,estado2 integer
+,idrol2 integer
+,rol varchar
+,descripcion2 varchar
+,estado3 integer
+
+);
+
+
+
+
+
+
