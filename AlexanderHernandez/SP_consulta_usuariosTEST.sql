e0ee694f0ab15ed34901720ae4da7883c1347a49
alhernand <kasashy@gmail.com>
2017-03-08 21:24:21 -0500

    Add files via upload
    Se sube Codigo de ejecucion de prueba el Sp de consulta de usuarios

---
+++ AlexanderHernandez/SP_consulta_usuariosTEST.sql
@@ -0,0 +1,21 @@
+select * from SP_consulta_usuarios() as usuario (
+idusuario integer
+,idtipodocumento integer
+,idrol integer
+,numerodocumento varchar
+,usuario varchar
+,clave varchar
+,nombre varchar
+,apellido varchar
+,estado integer
+,idtipodocumento2 integer
+,tipo varchar
+,descripcion varchar
+,estado2 integer
+,idrol2 integer
+,rol varchar
+,descripcion2 varchar
+,estado3 integer
+
+);
+
