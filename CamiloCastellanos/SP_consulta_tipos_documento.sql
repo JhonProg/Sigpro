dbd01e1e5e58c50b1ab4966db86fb030b1d64b05
camilo castellanos puentes <camilo.castellanos.puentes@gmail.com>
2017-03-10 17:47:31 -0500

    SPs #3 y #6

---
+++ CamiloCastellanos/SP_consulta_tipos_documento.sql
@@ -0,0 +1,9 @@
+﻿CREATE OR REPLACE FUNCTION SP_consulta_tipos_documento()
+RETURNS SETOF tipo_documento AS
+$BODY$
+ SELECT  idtipodocumento,tipo,descripcion, estado
+ FROM    tipo_documento
+ WHERE   estado =2;
+
+$BODY$
+LANGUAGE SQL;
---
+++ CamiloCastellanos/SP_consulta_usuario_por_id_usuario.sql
@@ -0,0 +1,12 @@
+﻿CREATE OR REPLACE FUNCTION  SP_consulta_usuario_por_id_usuario (idus INTEGER )
+RETURNS SETOF usuario AS
+
+$BODY$
+
+SELECT  idusuario, idtipodocumento,idrol, numerodocumento, usuario, 
+        clave, nombre, apellido, estado
+FROM usuario
+WHERE usuario.idusuario= idus
+
+$BODY$
+LANGUAGE SQL;
